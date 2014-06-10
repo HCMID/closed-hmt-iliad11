98bfe842bfd94027d3347217a555a4395954fbcb
oxygen XML editor <oxygen@halicarnassus.holycross.edu>
2014-06-10 15:45:45 -0400

    Add cd to verify

--- verify.sh
+++ verify.sh
@@ -1,3 +1,6 @@
 echo "Verifying from configuration in /vagrant/hmt-iliad11/vm-mom-config.gradle verify"
 
+
+cd /vagrant/hmt-mom
+
 gradle clean && gradle -Pconf=/vagrant/hmt-iliad11/vm-mom-config.gradle verify
