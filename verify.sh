ff22de004c56ca3ce519773c6441ec3a9b2c58c0
Neel Smith <dnsmith.neel@gmail.com>
2014-06-12 13:28:47 -0400

    Simplify interface to verify.sh

--- verify.sh
+++ verify.sh
@@ -1,6 +1,18 @@
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: sh verify.sh URN"
+    exit
+fi
+
+
 echo "Verifying from configuration in /vagrant/hmt-iliad11/vm-mom-config.gradle verify"
 
 
 cd /vagrant/hmt-mom
 
-gradle clean && gradle -Pconf=/vagrant/hmt-iliad11/vm-mom-config.gradle verify
+echo "Cleaning previous HMT MOM results..."
+gradle clean
+
+echo Beginning verification for folio $1
+
+gradle -Pfolio=$1 -Pconf=/vagrant/hmt-iliad11/vm-mom-config.gradle visinv
